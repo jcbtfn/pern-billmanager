4533c1be8e505d7f7dcda6f3b57fbae43cf1824e
jacobtfn <jacob.tfn@gmail.com>
2023-06-21 22:41:26 +0100

    edit-input-not-working

--- client/src/components/EditService.js
+++ client/src/components/EditService.js
@@ -5,6 +5,25 @@ const EditService = ({ service }) => {
     const [typeofservice, setService] = useState(service.typeofservice);
     const [provider, setProvider] = useState(service.provider);
 
+    //Edit description
+
+    const updateService = async e => {
+        e.preventDefault();
+        try {
+            const body = { service };
+            const response = await fetch(`http://localhost:5000/services/${service.serviceid}`, {
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            });
+
+            console.log(response);
+            
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return <Fragment>
 
         <button type="button" class="btn btn-warning" data-toggle="modal" data-target={`#id${service.serviceid}`}>
@@ -24,11 +43,15 @@ const EditService = ({ service }) => {
                         Service -change this-: <input type='text' className="form-control" value={typeofservice} 
                             onChange={e => setService(e.target.value)} />
                         Provider -change this-: <input type='text' className="form-control" value={provider}
-                            onChange={e => setService(e.target.value)} />
+                            onChange={e => setProvider(e.target.value)} />
                     </div>
 
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-warning" data-dismiss="modal">Edit</button>
+                        <button type="button" class="btn btn-warning" data-dismiss="modal"
+                            onClick={e => updateService(e)}
+                        >
+                            Edit
+                        </button>
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                     </div>
 
