ef5a089272fe499e328ee0e8bb3245f31ef9b871
jacobtfn <jacob.tfn@gmail.com>
2023-06-21 21:34:25 +0100

    updated list of services

--- client/src/components/InputBills.js
+++ client/src/components/InputBills.js
@@ -2,12 +2,13 @@ import React, { Fragment, useState } from 'react';
 
 const InputBill = () => {
 
-    const [typeOfService, setDescription] = useState("");
+    const [typeOfService, setTypeOfService] = useState("");
+    const [provider, setProvider] = useState("");
 
     const onSubmitForm = async (e) => {
         e.preventDefault();
         try {
-            const body = { typeOfService };
+            const body = { typeOfService, provider };
             const response = await fetch("http://localhost:5000/services", {
                 method: "POST",
                 headers: { "Content-type": "application/json" },
@@ -25,7 +26,8 @@ const InputBill = () => {
         <Fragment>
             <h1 className="text-center mt5">PERN - Bill Calculator and Log</h1>
             <form className='d-flex mt-5' onSubmit={onSubmitForm}>
-                <input type="text" className='form-control' value={typeOfService} onChange={e => setDescription(e.target.value)} />
+                <input type="text" className='form-control' value={typeOfService} onChange={e => setTypeOfService(e.target.value)} />
+                <input type="text" className='form-control' value={provider} onChange={e => setProvider(e.target.value)} />
                 <button className='btn btn-success'>Add</button>
             </form>
         </Fragment>
