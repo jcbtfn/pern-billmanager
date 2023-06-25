af9211f02bf439b77fb37e0b06e31f8a487976ab
jacobtfn <jacob.tfn@gmail.com>
2023-06-25 01:52:34 +0100

    routes bugfix

--- client/src/components/bills/EditBill.js
+++ client/src/components/bills/EditBill.js
@@ -7,23 +7,27 @@ const EditBill = ({ bill }) => {
     //const [provider, setProvider] = useState(service.provider);
 
     const [billAmount, setAmount] = useState(bill.amount);
-    const [billDate, setBillDate] = useState(bill.date);
+    const [billDate, setBillDate] = useState(bill.billdateformatted);
     const [isPaid, setIsPaid] = useState(bill.ispaid);
-    const [serviceId, setServiceId] = useState(bill.serviceid);
+    const [billServiceId, setBillServiceId] = useState(bill.serviceid);
 
     //Edit Bill
 
     const updateBill = async e => {
         e.preventDefault();
         try {
-            const body = { billAmount, billDate, isPaid, serviceId };
+            const body = { billAmount, billDate, isPaid, billServiceId };
             const response = await fetch(`http://localhost:5000/bills/${bill.billid}`, {
                 method: "PUT",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(body)
             });
 
-            window.location = "/";
+            console.log(response);
+
+            //window.location = "/";
+
+            console.log(response);
 
         } catch (err) {
             console.error(err.message);
@@ -32,14 +36,14 @@ const EditBill = ({ bill }) => {
 
     return <Fragment>
 
-        <button type="button" class="btn btn-warning" data-toggle="modal" data-target={`#id${bill.billid}`}>
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target={`#billid${bill.billid}`}>
             Edit
         </button>
 
         <div 
             class="modal" 
-            id={`id${bill.billid}`} 
-            onClick={() => (setAmount(bill.amount), setBillDate(bill.billdateformatted), setIsPaid(bill.ispaid), setServiceId(bill.serviceid))}
+            id={`billid${bill.billid}`} 
+            onClick={() => (setAmount(bill.amount), setBillDate(bill.billdateformatted), setIsPaid(bill.ispaid), setBillServiceId(bill.billServiceId))}
         >
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -47,7 +51,7 @@ const EditBill = ({ bill }) => {
                     <div class="modal-header">
                         <h4 class="modal-title">Edit Bill</h4>
                         <button type="button" class="close" data-dismiss="modal"
-                            onClick={() => (setAmount(bill.amount), setBillDate(bill.billdateformatted), setIsPaid(bill.ispaid), setServiceId(bill.serviceid))}>
+                            onClick={() => (setAmount(bill.amount), setBillDate(bill.billdateformatted), setIsPaid(bill.ispaid), setBillServiceId(bill.billServiceId))}>
                             &times;
                         </button>
                     </div>
@@ -55,12 +59,12 @@ const EditBill = ({ bill }) => {
                     <div class="modal-body">
                         Amount -change this-: <input type='text' className="form-control" value={billAmount} 
                             onChange={e => setAmount(e.target.value)} />
-                        Bill Date -change this-: <input type='date' className="form-control" value={billDate}
+                        Bill Date -change this-: <input type='text' className="form-control" value={billDate}
                             onChange={e => setBillDate(e.target.value)} />
                         Is Paid -change this-: <input type='text' className="form-control" value={isPaid} 
                             onChange={e => setIsPaid(e.target.value)} />
-                        ServiceId -change this-: <input type='text' className="form-control" value={serviceId}
-                            onChange={e => setServiceId(e.target.value)} />
+                        billServiceId -change this-: <input type='text' className="form-control" value={billServiceId}
+                            onChange={e => setBillServiceId(e.target.value)} />
                     </div>
 
                     <div class="modal-footer">
@@ -70,7 +74,7 @@ const EditBill = ({ bill }) => {
                             Edit
                         </button>
                         <button type="button" class="btn btn-danger" data-dismiss="modal"
-                            onClick={() => (setAmount(bill.amount), setBillDate(bill.billdateformatted), setIsPaid(bill.ispaid), setServiceId(bill.serviceid))}>
+                            onClick={() => (setAmount(bill.amount), setBillDate(bill.billdateformatted), setIsPaid(bill.ispaid), setBillServiceId(bill.billServiceId))}>
                             Close
                         </button>
                     </div>
